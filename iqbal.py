ed4d60e6a5a14d97a5996877984693cd652ca56b
Miqbal31 <m.iqbalmede@gmail.com>
2021-10-28 13:53:35 +0700

    Pratikum 1

---
+++ iqbal.py
@@ -0,0 +1,26 @@
+print ("Assalamualaikum")
+print ("Let me introduce my self")
+
+data = input("Nama Lengkap = ")
+print("My Name Is = ",data)
+
+data = input("Nama Panggilan = ")
+print("But You Can Call Me = ",data)
+
+data = input("NIM = ")
+print("My NIM Is = ",data)
+
+data = input("Tempat Lahir = ")
+print("I Was Born Is = ",data)
+
+data_int = int(input("Umur = "))
+print("Umur = ",data_int,"Years Old",)
+
+data = input("Alamat = ")
+print("Iam very glad if you wqant to invite my house in = ",data)
+
+data_int = int(input("No Telepon = "))
+print("So, Dont forget to call me before with the number = ",data_int)
+
+data = input("Hobi Saya = ")
+print("My Hobby = ",data)
